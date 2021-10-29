dc97b1800b0be6fa74c7686fba6b746f432a744f
Igor <igor.leahu24@gmail.com>
2021-10-29 11:04:15 +0300

    Allow to disable logger colors

--- src/modules/logger/logger.service.ts
+++ src/modules/logger/logger.service.ts
@@ -1,24 +1,24 @@
 import { LoggerService as LoggerServiceInterface } from '@nestjs/common';
-import { log } from './logger.util';
+import { logger } from './logger.util';
 
 export class LoggerService implements LoggerServiceInterface {
   log(message: any, ...rest: any[]) {
-    log('log')(rest.pop(), message, ...rest);
+    logger.log(rest.pop(), message, ...rest);
   }
 
   error(message: any, ...rest: any[]) {
-    log('error')(rest.pop(), message, ...rest);
+    logger.error(rest.pop(), message, ...rest);
   }
 
   warn(message: any, ...rest: any[]) {
-    log('warn')(rest.pop(), message, ...rest);
+    logger.warn(rest.pop(), message, ...rest);
   }
 
   debug?(message: any, ...rest: any[]) {
-    log('debug')(rest.pop(), message, ...rest);
+    logger.debug(rest.pop(), message, ...rest);
   }
 
   verbose?(message: any, ...rest: any[]) {
-    log('verbose')(rest.pop(), message, ...rest);
+    logger.verbose(rest.pop(), message, ...rest);
   }
 }
--- src/modules/logger/logger.util.ts
+++ src/modules/logger/logger.util.ts
@@ -1,10 +1,20 @@
 const RESET = '\x1b[0m';
+const BRIGHT = '\x1b[5m';
 const BOLD = '\x1b[1m';
 const CYAN = '\x1b[36m';
 const RED = '\x1b[31m';
 const YELLOW = '\x1b[33m';
 const GREEN = '\x1b[32m';
 const MAGENTA = '\x1b[35m';
+const BLUE = '\x1b[34m';
+
+const isColorLoggingEnabled = !process.env.NO_COLOR;
+const formatLevel = (color: string, level: string) =>
+  isColorLoggingEnabled ? `${color}${level}` : level;
+const formatContext = (context: string) =>
+  isColorLoggingEnabled ? `${BOLD}[${context}]${RESET}` : `[${context}]`;
+const formatMs = (ms: number) =>
+  isColorLoggingEnabled ? `${BRIGHT}${BLUE}+${ms}ms${RESET}` : `+${ms}ms`;
 
 const loggerColors = {
   log: GREEN,
@@ -14,21 +24,31 @@ const loggerColors = {
   verbose: CYAN,
 };
 
-const loggers = {};
-
+let prevTime = Number(new Date());
 const createLogger = (level: string, color: string) => {
-  const start = `${color}${level}`;
+  const start = formatLevel(color, level);
 
   return (context: string, ...messages: any[]) => {
-    const suffix = `${start} ${BOLD}[${context}]${RESET}`;
+    const suffix = `${start} ${formatContext(context)}`;
+    const currTime = Number(new Date());
+    const ms = currTime - (prevTime || currTime);
+    const timer = prevTime ? formatMs(ms) : '';
+    prevTime = currTime;
 
-    console.log(`${suffix}`, ...messages);
+    console.log(`${suffix}`, ...messages, timer);
   };
 };
 
-export const log = (level) => {
-  if (!loggers[level]) {
-    loggers[level] = createLogger(level.toUpperCase(), loggerColors[level]);
-  }
-  return loggers[level];
-};
+const _logger: {
+  [key: string]: (context: string, ...messages: any[]) => void;
+} = {};
+
+export const logger = new Proxy(_logger, {
+  get(target, prop: string) {
+    if (!_logger[prop]) {
+      _logger[prop] = createLogger(prop.toUpperCase(), loggerColors[prop]);
+    }
+
+    return _logger[prop];
+  },
+});
