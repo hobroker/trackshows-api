b498b4dcccc3e4d16236775bc5706e2a74c2875e
Igor <igor.leahu24@gmail.com>
2021-11-03 01:06:34 +0200

    Update tmdb-genre.service.ts

--- src/modules/tmdb/services/tmdb-genre.service.ts
+++ src/modules/tmdb/services/tmdb-genre.service.ts
@@ -1,13 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { map, applySpec, prop } from 'ramda';
 import { PrismaService } from '../../prisma';
 import { HttpService } from '../../http';
-import { Genre } from '../entities/genre';
-
-const genreFacade = applySpec<Genre>({
-  externalId: prop('id'),
-  name: prop('name'),
-});
 
 @Injectable()
 export class TmdbGenreService {
@@ -22,14 +15,5 @@ export class TmdbGenreService {
     const { genres } = data;
 
     return genres;
-  }
-
-  async syncList() {
-    const genres = await this.list().then(map(genreFacade));
-
-    await this.prismaService.genre.createMany({
-      data: genres,
-      skipDuplicates: true,
-    });
   }
 }
