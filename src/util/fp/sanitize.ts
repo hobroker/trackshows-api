4331935ace19ed2f2d64818776afaea3dddbee17
Igor Leahu <igor.leahu24@gmail.com>
2022-03-12 21:25:56 +0200

    feat(deps): update dependencies (#35)
    BREAKING CHANGE: update rambda and GraphQL dependencies

--- src/util/fp/sanitize.ts
+++ src/util/fp/sanitize.ts
@@ -1,7 +1,3 @@
 import { compose, replace, trim } from 'rambda';
 
-export const sanitize = compose<string, string, string, string>(
-  trim,
-  replace(/\s+/g, ' '),
-  replace(/\n/g, ' '),
-);
+export const sanitize = compose(trim, replace(/\s+/g, ' '));
