34f1a682f4ff3b64dbe07647dd4356dcd9835b28
Igor <4361921+hobroker@users.noreply.github.com>
2022-03-26 00:19:16 +0200

    feat: add v1 prefix to REST API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { Logger, RequestMethod } from '@nestjs/common';
 import { LoggerService } from './modules/logger';
 import { AppModule } from './app.module';
 import { APP_MODULE_ID } from './app.constants';
@@ -10,6 +10,10 @@ async function bootstrap() {
     logger: new LoggerService(),
   });
   app.enableCors();
+  app.setGlobalPrefix('v1', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+
   const logger = new Logger('bootstrap');
   const configService = app.get(ConfigService);
   const { port } = configService.get(APP_MODULE_ID);
