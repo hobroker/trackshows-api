8235de5c48336c575e3bb16aa524c6f75b58001e
Igor <4361921+hobroker@users.noreply.github.com>
2022-04-10 21:16:39 +0300

    Update auth.util.ts

--- src/modules/auth/auth.util.ts
+++ src/modules/auth/auth.util.ts
@@ -1,4 +1,4 @@
 export const createCookie = (
   name: string,
   { value = '', maxAge = '0', path = '/' } = {},
-) => `${name}=${value}; Path=${path}; Max-Age=${maxAge}`;
+) => `${name}=${value}; Path=${path}; Max-Age=${maxAge}; SameSite=None; Secure`;
